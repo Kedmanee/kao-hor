c2d24036bf36954da655a837b07cddb311cc502d
Kedmanee <73660985+Kedmanee@users.noreply.github.com>
2020-12-08 15:49:22 +0700

    Add files via upload

--- enter.php
+++ enter.php
@@ -46,7 +46,7 @@
             <a class="nav-link" href="profile.html">ข้อมูลผู้ใช้</a>
           </li>
           <li class="nav-item">
-            <a href="index.php?logout='1'">ออกจากระบบ</a>
+            <a href="enter.php?logout='1'">ออกจากระบบ</a>
           </li>
         </ul>
       </div>
